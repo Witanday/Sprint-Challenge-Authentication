6208396678ef7cc00c90743a157c287f81d3a665
Witanday <witanday.cd@gmail.com>
2019-01-31 22:55:08 -0500

    implement user registration and loggin

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,12 @@
 const axios = require('axios');
-
+require('dotenv').config();
 const { authenticate } = require('../auth/authenticate');
+
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const db = require('../database/dbConfig');
+
 
 module.exports = server => {
   server.post('/api/register', register);
@@ -8,12 +14,62 @@ module.exports = server => {
   server.get('/api/jokes', authenticate, getJokes);
 };
 
+function initiateToken(username){
+const payload = {
+  username,
+}
+
+const secret = process.env.JWT_SECRET;
+
+const options = {
+  expiresIn : '20m'
+}
+
+return jwt.sign(payload, secret, options);
+}
+
+function hashPassword(cred){
+  const hash = bcrypt.hashSync(cred.password, 14);
+  return cred.password = hash;
+}
+
+
+
 function register(req, res) {
   // implement user registration
+  const cred = req.body;
+  
+  hashPassword(cred);
+db('users').insert(cred)
+.then(id =>{
+  const token = initiateToken(cred.username)
+  res.status(201).json({id,token })
+})
+.catch(err =>{
+  res.status(500).json(err)
+})
+
+
 }
 
 function login(req, res) {
   // implement user login
+  const cred = req.body;
+
+db('users').where({username : cred.username }).first()
+.then(user =>{
+  if( user && bcrypt.compareSync(cred.password, user.password)){
+    const token = initiateToken(cred.username)
+    res.status(201).json({Message :`Welcome ${cred.username}`, token })
+  }
+  else  {
+    return res.status(401).json({ error: 'Incorrect credentials' });
+  }
+  
+})
+.catch(err =>{
+  res.status(500).json(err)
+})
 }
 
 function getJokes(req, res) {
